c4b66dea7db3276ce55c4a3c932154d3eed1f6e3
kenjikatooo <k.524.10nis@gmail.com>
2018-08-02 12:30:43 +0900

    シンプルな文章を表示できるようにしました

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// create a new component. this component should produce some HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = function() {
+  return <div>Hi!!</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
