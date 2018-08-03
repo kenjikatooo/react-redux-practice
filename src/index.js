bf45101f5c99f205e5b54b7e481437a6cd4950d9
kenjikatooo <k.524.10nis@gmail.com>
2018-08-04 00:27:41 +0900

    add commentouts

--- src/index.js
+++ src/index.js
@@ -7,4 +7,6 @@ const App = function() {
   return <div>Hi!!</div>;
 }
 
+// take this component's  generated HTML and put it
+// on the page ( in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
