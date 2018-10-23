958890cbdc5c1821b4c9da148398065c50aaa25f
kenjikatooo <k.524.10nis@gmail.com>
2018-10-23 22:32:51 +0900

    update sercchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />
-};
 
-/* class SearchBar extends React.Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: '' };
+    this.state = {term: ''};
   }
 
   render() {
     return (
     <div>
       <input
-        value = {this.state.term}
-        onChange={(event) => this.setState({ term: event.target.value })} />
+        value = {this.state.term} 
+        onChange={(event) => this.setState({ term: event.target.value})} />
     </div>
     );
   }
 }
- */
+
 
 export default SearchBar;
