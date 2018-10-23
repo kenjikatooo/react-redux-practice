e0cd6b30aee51aeff711b868cab62f9e45c962c7
kenjikatooo <k.524.10nis@gmail.com>
2018-10-23 23:06:05 +0900

    uopdate serchbar

--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
 // create a new component. this component should produce some HTML
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return(
       <div>
